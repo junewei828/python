9bc53374c1055a6ea6282bb3341fcd4c5fdbf187
junewei828 <47428741+junewei828@users.noreply.github.com>
2019-06-22 07:49:42 -0700

    Delete selenium_test.py

--- selenium/selenium_test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-# chromedriver = '/usr/local/bin/chromedriver'
-driver = webdriver.Firefox()
-# driver = webdriver.Chrome()
-driver.get("https://python.org")
-driver.find_element_by_id('')
-y.text
